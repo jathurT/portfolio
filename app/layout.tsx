cc7a0c1f7484c85b021ad2b6921c8b36d529b378
jathurT <ktmjathur2001@gmail.com>
2025-12-11 15:14:56 +0530

    added google anlytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N7SMM659P9"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N7SMM659P9');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
